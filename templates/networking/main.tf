df8d17fc4b51346c2c6b270911c439d0b221c552
Lucas4lves <zombnfl@gmail.com>
2026-05-07 17:30:25 -0300

    feature(iac): networking base template v1

---
+++ templates/networking/main.tf
@@ -0,0 +1,19 @@
+resource "aws_vpc" "this" {
+  cidr_block = var.vpc_cidr
+  enable_dns_hostnames = true
+  tags = { Name = var.vpc_name }
+}
+
+resource "aws_subnet" "public" {
+  count = length(var.public_subnets_cidr)
+  vpc_id = aws_vpc.this.id
+  cidr_block = var.public_subnets_cidr[count.index]
+  availability_zone = var.azs[count.index]
+
+  tags = { Name = "${var.vpc_name}-public-${count.index}" }
+}
+
+resource "aws_internet_gateway" "this" {
+  vpc_id = aws_vpc.this.id
+  tags = { Name = "${var.vpc_name}-igw" }
+}
